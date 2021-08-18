eb8b8b16915201bb9df050a0275e08e86f09be2e
Paul Moisy <moisypaul@gmail.com>
2021-08-18 19:48:13 +0000

    deleted two data rows

--- UCDPA_PAUL_MOISY.py
+++ UCDPA_PAUL_MOISY.py
@@ -11,12 +11,12 @@ import pandas as pd
 df = pd.read_csv("Cell_Utilization_1.csv")
 
 #Checking if data set is loaded.
-print(df.head())
+df.head(10)
 
 #looking at data set to determine if data is in the correct format.
-#Also checking to see if there is any missing values.
 df.info()
 
+#Some of the headings have unwanted index charicters from the export. 
 #Removing unwanted charicters from Column headings.
 def run():
     lis = ['ubk', 'xaw', 'lml', 'jmo', 'lme']
@@ -34,36 +34,41 @@ df.info()
 df.drop_duplicates(inplace=True)
 df.shape
 
-#Checking to see if there is any null values.
-#Column: "GoodQuantity" containes 116 null values, "ActualEndTime" and "actualHoursOnTCard"
-#both contain 3. The null values in "GoodQuantity can be replaced with 0.
-#However the values for "ActualEndTime" and "actualHoursOnTCard" will need to be considered.
-df.isna().sum()
-
-#Replacing null values in column: "GoodQuantity" with "0" and checking the result. 
-df = df.fillna({'GoodQuantity':'0'})
-df.isna().sum()
-
-
-
 #Checking to see if only cell operators are within the dataset.
 #If there is aditional operators within the dataset it indicates that they logged on to the incorrect machine.
 print(df['EmployeeName'])
 
-Cell_2_operatives = ['Shaun Murphy', 'Leo Cunniffe', 'Murray Pascall', 'Fernando Toni']
+#Cell_2_operatives = ['Shaun Murphy', 'Leo Cunniffe', 'Murray Pascall', 'Fernando Toni']
 
 #Applying filter to dataset to only show the relevant operatives.
 #Dataset was renamed "cell_2" as an analysis of incorrect loggin's is required on "df"
 cell_2 = df[(df.EmployeeName == 'Leo Cunniffe') | (df.EmployeeName == 'Shaun Murphy') | 
         (df.EmployeeName == 'Murray Pascall') |  (df.EmployeeName == 'Fernando Toni')]
 
+#Conferming that unwanted operators are removed.
+print(cell_2['EmployeeName'])
 
-leo = df[df.EmployeeName == 'Leo Cunniffe']
-shaun = df[df.EmployeeName == 'Shaun Murphy']
-murray = df[df.EmployeeName == 'Murray Pascall']
-fernando = df[df.EmployeeName == 'Fernando Toni']
+#Checking to see if there is any null values.
+#Column: "GoodQuantity" containes 86 null values, "ActualEndTime" and "actualHoursOnTCard"
+#both contain 3. The null values in "GoodQuantity can be replaced with 0.
+#However the values for "ActualEndTime" and "actualHoursOnTCard" will need to be considered.
+print(cell_2.isna().sum())
 
+#Replacing null values in column: "GoodQuantity" with "0" and checking the result. 
+cell_2 = cell_2.fillna({'GoodQuantity':'0'})
+print(cell_2.isna().sum() / len(cell_2)*100)
 
+#The remaining two missing values equate to 0.43% of the total in that column.
+#Thus deleting the rows will not significently alter the analysis results.
+print(cell_2.dropna().info())
+
+#Creating seperate datasets for each operative.
+leo = cell_2[cell_2.EmployeeName == 'Leo Cunniffe']
+shaun = cell_2[cell_2.EmployeeName == 'Shaun Murphy']
+murray = cell_2[cell_2.EmployeeName == 'Murray Pascall']
+fernando = cell_2[cell_2.EmployeeName == 'Fernando Toni']
+
+#Playing around with graphs.
 import matplotlib.pyplot as plt
 
 plt.xlabel('Date')
@@ -77,7 +82,10 @@ plt.plot(cell_2.directEarnedHours)
 
 import seaborn as sns
 
-#sns.scatterplot(data=cell_2, x='weekDay', y='directEarnedHours' )
+#This scatter plot shows some interesting 
+#sns.histplot(data=cell_2, x='weekDay', y='directEarnedHours' )
+
+
 
 
 
